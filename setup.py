07165ddb347b2c4cbec09f3888a0416377f401b5
SelmiAbderrahim <codingwithselmi@gmail.com>
2022-05-25 16:07:09 -0400

    tested on Linux

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.8' 
+VERSION = '0.0.9' 
 DESCRIPTION = 'Create Local Undetectable Chrome Driver'
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
