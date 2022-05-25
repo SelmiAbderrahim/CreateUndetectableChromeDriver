03890c9b7a13065de7f9e3a587fe97fcdc82a578
SelmiAbderrahim <codingwithselmi@gmail.com>
2022-05-25 16:00:53 +0100

    ImportError

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2' 
+VERSION = '0.0.3' 
 DESCRIPTION = 'Create Local Undetectable Chrome Driver'
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
